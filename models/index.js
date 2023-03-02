66c62d8df9f9b1ccf0e7844324ae964e345d90a6
Mona Lisa <tweetwee1996@gmail.com>
2023-03-01 21:12:25 -0800

    controller and models and suggestion books db

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const Card = require('./Card');
-
+const Books = require('/.Books')
 User.hasOne(Card, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
@@ -10,6 +10,7 @@ Card.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Card };
+//Books.
+module.exports = { User, Card, Books};
 
 //needed to double check
