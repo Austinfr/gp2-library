169221e5e94b6703732dd03129fdc9cc6b5e4d4f
Kevin Lang <kevinl20@yahoo.com>
2023-03-06 11:58:11 -0800

    added container to handlebar

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -11,6 +11,32 @@ router.get('/', withAuth, async (req, res) => {
         res.status(500).json(err);
     }
 });
+//routes to get all books //
+
+// router.get('/', async (req, res) => {
+//     const bookData = await Book.findAll().catch((err) => {
+//     res.json(err);
+// });
+// const books = bookData.map((book) => { book.get({ plain: true }); });
+//     res.render('all', { books });
+// });
+
+
+// route to get a single book //
+
+// router.get('/book/:id', async (req, res) => {
+//     try{
+//         const bookData = await Book.findByPk(req.params.id,); 
+//             if(!bookData){
+//                 res.status(404).json({ message: 'No book found with this id'});
+//                 return;
+//     }
+//     const book = bookData.get({ plain: true });
+//     res.render('book', book);
+// } catch (err) {
+//     res.status(500).json(err);
+// };
+//     });
 
 router.get('/login', async (req, res) => {
     if(req.session.loggedIn){
