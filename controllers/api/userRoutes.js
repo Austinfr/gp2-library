ffbdbe037f6471086ecc3fb505780c2ceeb4341e
Austie <wustinah20@gmail.com>
2023-03-05 21:37:06 -0800

    udpated routes and models. Need to figure out how to map a user to a card

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -20,8 +20,7 @@ router.post('/login', async (req, res, next) => {
         const userData = await User.findOne({ where: { email: req.body.email } });
 
         if (!userData) {
-        res
-            .status(400)
+        res.status(400)
             .json({ message: 'Incorrect email or password, please try again' });
         return;
         }
@@ -29,12 +28,11 @@ router.post('/login', async (req, res, next) => {
         const validPassword = await userData.checkPassword(req.body.password);
 
         if (!validPassword) {
-        res
-            .status(400)
+        res.status(400)
             .json({ message: 'Incorrect email or password, please try again' });
         return;
         }
-        
+
         req.session.save(() => {
             req.session.user_id = userData.id;
             req.session.logged_in = true;
--- controllers/searchRoutes.js
+++ controllers/searchRoutes.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
+const { Card } = require('../models');
 
 router.get('/', async (req, res) => {
     try {
         const cardData = await Card.findAll({
           where: { user_id: req.session.user_id },
         });
+        console.log(cardData);
         const cards = cardData.map((card) => card.get({ plain: true }));
         res.render('search', { username: req.user.username, cards });
       } catch (err) {
